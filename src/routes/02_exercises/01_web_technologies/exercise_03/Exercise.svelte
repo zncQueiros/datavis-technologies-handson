257346cd31f0bc227904ebd37b952172b16428ab
zncQueiros <zncqueiros@outlook.com>
2023-03-24 17:07:54 +0100

    Done

---
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -0,0 +1,13 @@
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
+    <head>
+    </head>
+    <body>
+        <p>The best visual channels ordered by Mackinlay's ranking:</p>
+        <ol>
+            <li>position</li>
+            <li>length</li>
+            <li>angle</li>
+        </ol>
+    </body>
+</html>
