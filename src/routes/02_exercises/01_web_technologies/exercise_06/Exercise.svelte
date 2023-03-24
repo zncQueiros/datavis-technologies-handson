3d713c1b2015abce97a81898605f0243a5ad6d00
zncQueiros <zncqueiros@outlook.com>
2023-03-24 17:27:15 +0100

    Done

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,9 @@
-I really like data visualisation!
+<div class="p">I <em class="q">really</em> like data visualisation!</div> 
+<style>
+    div.p {
+      color: steelblue;
+    }
+    .q {
+      color: red;
+    }
+</style>
