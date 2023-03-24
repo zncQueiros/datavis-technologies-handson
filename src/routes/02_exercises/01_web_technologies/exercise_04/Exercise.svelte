abe6f170de627580cd763cdbeb43c7f745aad48b
zncQueiros <zncqueiros@outlook.com>
2023-03-24 17:12:00 +0100

    Done

--- src/routes/02_exercises/01_web_technologies/exercise_04/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_04/Exercise.svelte
@@ -1,1 +1,8 @@
-Marks are basic geometric elements that depict items and links.
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
+    <head>
+    </head>
+    <body>
+        <p><em>Marks</em> are <del>basic</del> <b>geometric elements</b> that depict items and links.</p>
+    </body>
+</html>
