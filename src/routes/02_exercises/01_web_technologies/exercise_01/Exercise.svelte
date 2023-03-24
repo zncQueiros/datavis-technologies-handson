2365b393e77ba12b1524b0c66530424680898227
zncQueiros <zncqueiros@outlook.com>
2023-03-24 16:56:57 +0100

    1-7

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,7 +1,7 @@
-<div class="answer">My name is ...</div>
+<div class="answer">My name is <b>Naichuan Zhang</b></div>
 
 <style>
   div.answer {
-    color: rgb(2, 96, 45);
+    color: red;
   }
 </style>
