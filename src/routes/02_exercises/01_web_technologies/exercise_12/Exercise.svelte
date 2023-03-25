20d223ac3e16f786b405a259e1bf09915b894e7b
zncQueiros <zncqueiros@outlook.com>
2023-03-25 14:48:31 +0100

    Done

--- src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
@@ -6,5 +6,9 @@
 	<button class="btn btn-primary" on:click={() => (clicked = true)}>
 		click here
 	</button>
-	<!--  -->
+	{#if clicked == true} 
+		You have clicked the button!
+	{:else}
+		You have to click the button! 
+	{/if}
 </div>
