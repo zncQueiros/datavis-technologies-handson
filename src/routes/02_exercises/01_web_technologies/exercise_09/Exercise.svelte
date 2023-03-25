58ae74f188f1249536925bb3b3e54676e0a9614a
zncQueiros <zncqueiros@outlook.com>
2023-03-25 12:13:17 +0100

    Done

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,9 @@
+
+<script>
+    function cir(r) {
+        return 2*3.14*r
+    }
+    const r = 3;
+    console.log("The circumference of a circle with radius " + r + "m is " + cir(r)+ "m .");
+</script>
+<p>The circumference of a circle with radius {r}m is {cir(r)}m .</p>
