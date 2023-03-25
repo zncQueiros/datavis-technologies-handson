4e0f71ab1d936be167bceda56caffe50cd466670
zncQueiros <zncqueiros@outlook.com>
2023-03-25 12:44:02 +0100

    Done

---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Exercise3 from "../exercise_03/Exercise.svelte";
+    import Exercise2 from "../exercise_02/Exercise.svelte";
+</script>
+
+<div>
+    <Exercise3 />
+</div>
+
+<div>
+    <Exercise2 />
+</div>
