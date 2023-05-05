632c673bf58020156b57b4746c5e8f0fe0c1d353
zncQueiros <zncqueiros@outlook.com>
2023-05-05 14:27:48 +0200

    Done

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -4,18 +4,25 @@
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
-  </script>
+</script>
   
-  <svg {width} {height}>
-    <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
-    </g>
-  </svg>
+<svg {width} {height}>
+  <g transform="translate({margin.left}, {margin.top})">
+    <rect x=25 y=25 width=40 height=40 />
+    <circle cx=75 cy=75 r=20 />
+  </g>
+</svg>
   
-  <style>
-    svg {
-      border: 1px solid black;
-      border-radius: 5px;
-    }
-  </style>
+<style>
+  svg {
+    border: 1px solid black;
+    border-radius: 5px; 
+  }
+  rect {
+    fill: yellow;
+  }
+  circle {
+    fill: skyblue;
+  }
+</style>
   
