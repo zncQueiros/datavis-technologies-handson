b6c196543d039b6cbabf11aa9b4b19a329e70c0e
zncQueiros <zncqueiros@outlook.com>
2023-05-05 16:39:38 +0200

    Done

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -1,10 +1,15 @@
 <script>
+    import {scaleLinear} from 'd3-scale';
+    import {scaleLog} from 'd3-scale';
     // Dimensions
     const width = 800;
     const height = 100;
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+    const logscaler = scaleLog()
+                      .domain([1,10])
+                      .range([0,innerWidth]);
   
     // Array
     const values = [2, 4, 6, 7, 9];
@@ -13,6 +18,8 @@
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
       <!--  -->
+      {#each values as value}
+       <circle cx={logscaler(value)} cy={innerHeight/2} r="10" fill='teal'/>
+      {/each}
     </g>
   </svg>
-  
