2d84cacee641d434d11859e7ba563059fb2bdfa6
zncQueiros <zncqueiros@outlook.com>
2023-05-05 16:45:08 +0200

    Done

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -6,9 +6,7 @@
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
-    const logscaler = scaleLog()
-                      .domain([1,10])
-                      .range([0,innerWidth]);
+    const logscaler = scaleLog().domain([1,10]).range([0,innerWidth]);
   
     // Array
     const values = [2, 4, 6, 7, 9];
@@ -16,7 +14,6 @@
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
       {#each values as value}
        <circle cx={logscaler(value)} cy={innerHeight/2} r="10" fill='teal'/>
       {/each}
