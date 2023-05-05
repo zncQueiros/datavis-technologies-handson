092dd09d4bfae10dc93b2d3a5be1ea2a9cf327b1
zncQueiros <zncqueiros@outlook.com>
2023-05-05 20:45:32 +0200

    Done

--- src/routes/02_exercises/02_basic_visualisations/exercise_19/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_19/Exercise.svelte
@@ -1,10 +1,19 @@
 <script>
+  import {scaleLinear} from "d3-scale";
+  import {scaleOrdinal} from "d3-scale"
+  import {extent} from "d3-scale"
+  import {schemeDark2} from "d3-scale-chromatic"
     // Dimensions
     const width = 800;
     const height = 100;
     const margin = { top: 20, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+
+    const linearscalerx = scaleLinear().domain([1,10]).range([0,innerWidth]);
+    const linearscalery = scaleLinear().domain([1,3]).range([0,innerHeight]);
+    const classcolor = extent(values,(d)=>d.category);
+    const scalecolor = scaleOrdinal().domain(classcolor).range(schemeDark2);
   
     // Array
     const values = [
@@ -18,7 +27,11 @@
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+        <circle cx={linearscalerx(value.x)} cy={linearscalery(value.y)-10} radius="5" fill={scalecolor(value.category)}></circle>
+        <text x={linearscalerx(value.x)} y={linearscalery(value.y)-10}>{value.y}</text>
+        <line x1={linearscalerx(value.x)} x2={linearscalerx(value.x)} y1={linearscalery(value.y)} y2={height} stroke="black"></line>
+      {/each}
     </g>
   </svg>
   
@@ -29,3 +42,4 @@
     }
   </style>
   
+  <li>{classcolor}</li>
